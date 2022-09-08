4a8d9f76e1bf472385e06cc967f21df021e86b29
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-09-08 07:20:25 +0300

    Milestone: Able to fetch and map data in a table

--- client/internship_management_system/src/components/Interns.js
+++ client/internship_management_system/src/components/Interns.js
@@ -1,10 +1,48 @@
 import React from "react";
+import Table from 'react-bootstrap/Table';
+import { useEffect, useState } from "react";
+
+
 
 function Interns (){
 
+
+    const[interns, setInterns] = useState([])
+
+    useEffect(() => {
+    
+        fetch("http://localhost:9292/interns")
+    
+        .then(res => res.json())
+        .then(data => {
+          setInterns(data)
+        })
+      },[])
+
+    //   console.log(interns)
+
     return (
         <div>
-            This is the interns page
+            <div>This is the interns page</div>
+            <div>
+            <Table striped bordered hover size="sm">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>Intern's Name</th>
+          <th>Intern's Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        {interns.map(intern => <tr key={intern.id}>
+                <td>{intern.id}</td>
+                <td>{intern.name}</td>
+                <td>{intern.email}</td>
+            </tr>)}
+      </tbody>
+    </Table>
+
+            </div>
         </div>
     )
 }
